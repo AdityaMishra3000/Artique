c860acd793002bbb7980935b7e5c86303e4c90a2
AdityaMishra3000 <mr.ba8man@gmail.com>
2025-09-25 10:58:47 +0530

    React implemented

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+// function App() {
+//   return (
+//     <div className="bg-gray-900 text-white min-h-screen">
+//       <h1 className="text-3xl font-bold text-center p-8">
+//         Artique Frontend
+//       </h1>
+//       {/* All our future components and pages will be rendered from here */}
+//     </div>
+//   );
+// }
+
+// export default App;
